a33a00029a6b72ae91652eb108a493e4245acf3b
Guyot Eliott <o22306727@or-iut-i102-f23.iut45.univ-orleans.fr>
2025-09-04 09:58:46 +0200

    ajout des pages aboutus, contact us et la difference entre home avec et sans parametres

--- TutoDjango/monApp/urls.py
+++ TutoDjango/monApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('home/',views.home ,name='home'),
+    path('homeAv/<param>',views.homeAv ,name='homeAv'),
+    path("aboutus", views.aboutus, name="aboutus"),
+    path("contactus", views.contactus, name="contactus"),
 ]
--- TutoDjango/monApp/views.py
+++ TutoDjango/monApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django .http import HttpResponse
+def home(request  ):
+    return HttpResponse ("<h1>bonsoir<h1>")
+def homeAv(request , param ):
+    return HttpResponse ("<h1>"+param+"<h1>")
+            
 
-def home(request):
-    return HttpResponse("<h1>Hello Django!</h1>")
-# Create your views here.
+def aboutus(request):
+    return HttpResponse("<h1>à propos de nous!</h1>")
+def contactus(request):
+    return HttpResponse("<h1>nous contacter!</h1>")
+
