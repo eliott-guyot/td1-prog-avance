73939d2cbe69865d916ca316000117f56bbbf704
Guyot Eliott <o22306727@or-iut-i102-f23.iut45.univ-orleans.fr>
2025-09-04 10:49:18 +0200

    ajout des excercice demandé fait

--- TutoDjango/monApp/models.py
+++ TutoDjango/monApp/models.py
@@ -4,12 +4,20 @@ class Categorie(models.Model):
     nomCat = models.CharField(max_length=100)
     def __str__(self):
         return self.nomCat  
+class Statut(models.Model):
+    idStatut=models.AutoField(primary_key=True)
+    libelléStatut=models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.idStatut} pour : {self.libelléStatut}"
+
 class Produit(models.Model):
     refProd = models.AutoField(primary_key=True)
     intituleProd = models.CharField(max_length=200)
     prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
+    dateDeFabrication=models.DateField(auto_now=True)
     # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie 1,1 côté produit)→
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
+    statut=models.ForeignKey(Statut,on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
     def __str__(self):
         return self.intituleProd
 class Rayon(models.Model):
@@ -26,3 +34,4 @@ class Contenir(models.Model):
         unique_together=("nomprod","nomRayon")
     def __str__(self):
         return f"{self.nomprod} dans {self.nomRayon} pour {self.qte}"
+
