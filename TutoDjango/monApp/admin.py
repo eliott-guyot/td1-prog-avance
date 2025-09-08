959594c3b68dcfbd6da6ce83eedfa9b1e30839b7
Guyot Eliott <o22306727@or-iut-i207-f23.iut45.univ-orleans.fr>
2025-09-08 16:57:37 +0200

    modif du fichier admin

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Produit,Categorie,Statut,Rayon,Contenir
 
-# Register your models here.
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ('intituleProd', 'prixUnitaireProd')
+admin.site.register(Produit, ProduitAdmin)
+
+admin.site.register(Categorie)
+admin.site.register(Statut)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
+
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
