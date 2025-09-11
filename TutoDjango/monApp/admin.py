0138d243f20acaf13759c17d39ee5cb3fdb2d0bb
eliott-guyot <eliott.guyot@etu.univ-orleans.fr>
2025-09-11 10:05:36 +0200

    modif de produit pour pouvoir filtrer

--- TutoDjango/monApp/admin.py
+++ TutoDjango/monApp/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 from .models import Produit,Categorie,Statut,Rayon,Contenir
 
+
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_status'
+    def lookups(self, request, model_admin) :
+        return (
+        ('OnLine', 'En ligne'),
+        ('OffLine', 'Hors ligne'),
+        )
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(statut=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(statut=0)
+
+
 class ProduitAdmin(admin.ModelAdmin):
     model = Produit
     list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateDeFabrication", "categorie", "statut"]
-    list_editable = ["intituleProd", "prixUnitaireProd", "dateDeFabrication"]
+    list_editable = ["intituleProd", "prixUnitaireProd", "dateDeFabrication","statut"]
     radio_fields = {"statut": admin.VERTICAL}
     search_fields = ('intituleProd', 'dateDeFabrication')
-    
-admin.site.register(Produit, ProduitAdmin)
+    list_filter = (ProduitFilter,)
+    date_hierarchy = 'dateDeFabrication'
 
+admin.site.register(Produit, ProduitAdmin)
 class ProduitInline(admin.TabularInline):
     model = Produit
     extra = 1 # nombre de lignes vides par défaut
